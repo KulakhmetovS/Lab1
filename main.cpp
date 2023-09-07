97ad12f6d8013f4727975a1f5cd93b4615ed18d1
KulakhmetovS <133137110+KulakhmetovS@users.noreply.github.com>
2023-09-07 12:38:50 +0300

    version 0.2

--- main.cpp
+++ main.cpp
@@ -4,20 +4,57 @@
 
 using namespace std;
 
+
+void MaxMinArray();
+void RandArray();
+void DinamicArray();
+void SummArray();
+void StructSearch();
+
+
 int main()
 {
-    int arr[array_size];
+    int action = 0;
 
-    for(int i = 0; i < array_size; i++)
+    cout << "Press \"Alt+F4\" to quit" << endl;
+
+    while(1)
     {
-        arr[i] = rand() % 100;
+        cout << "Choose the task: ";
+        cin >> action;
+        if ((action < 1) || (action > 5)) {cout << "Invalid operation" << endl;}
+        else if(action == 1) MaxMinArray();
+        else if(action == 2) RandArray();
+        else if(action == 3) DinamicArray();
+        else if(action == 4) SummArray();
+        else if(action == 5) StructSearch();
     }
-
-    for(int j = 0; j < array_size; j++)
-    {
-        cout << arr[j] << endl;
-    }
-
 
     return 0;
+}
+
+
+void MaxMinArray()
+{
+    cout << "Task 1" << endl;
+}
+
+void RandArray()
+{
+    cout << "Task 2" << endl;
+}
+
+void DinamicArray()
+{
+    cout << "Task 3" << endl;
+}
+
+void SummArray()
+{
+    cout << "Task 4" << endl;
+}
+
+void StructSearch()
+{
+    cout << "Task 5" << endl;
 }
